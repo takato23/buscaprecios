6b941f13362085fdc6eed4f59190efd163f3de23
takato23 <63169474+takato23@users.noreply.github.com>
2025-04-11 03:07:54 -0300

    Update cors.go

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -7,12 +7,13 @@ import (
 
 func CORS(router *gin.Engine) {
     corsConfig := cors.Config{
-        AllowOrigins:     []string{"http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176", "http://localhost:5177"}, 
-        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-        AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
-        AllowCredentials: true,
-        AllowWildcard:    true,
-    }
+    // AllowOrigins:     []string{"http://localhost:5173", "http://localhost:5174"}, // Comentado temporalmente
+    AllowAllOrigins:  true, // <-- Añadir esto temporalmente
+    AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+    AllowCredentials: true,
+    // AllowWildcard:    true, // Esto podría ser redundante con AllowAllOrigins
+}
 
     router.Use(cors.New(corsConfig))
 }
